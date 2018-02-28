eae61ea4302b983cab2fe51a7440dbea5aad0384
mark olsen <olsen.mark@gmail.com>
2018-02-28 21:28:07 +1300

    add code for an endpoint for writing to dynamodb

--- index.ts
+++ index.ts
@@ -1,24 +1,54 @@
 import {Request, Response} from 'express';
 import {Hello} from './src/Hello';
 import {Config} from './src/Config';
+import AWS = require('aws-sdk');
+import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
 const serverless = require('serverless-http');
-const express = require('express')
+const express = require('express');
+
+AWS.config.update({region: "us-east-1"});
+const dynamoDb: DocumentClient = new AWS.DynamoDB.DocumentClient();
+
+
 
 const app = express()
 
 app.get('/', function (req: Request, res: Response) {
   let hello = new Hello('World');
   res.send(hello.greet())
-})
+});
 
 app.get('/goodbye', (req: Request, res: Response) => {
   let goodbye = new Hello('Goodbye');
   res.send(goodbye.greet());
-})
+});
 
 app.get('/config', (req: Request, res: Response) => {
   let config = new Config();
   res.send(config.values);
+});
+
+app.get("/write/:id/:text", (req: Request, res: Response) => {
+  console.log(req.params.id);
+  console.log(req.params.text);
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+   // TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: req.params.id,
+      text: { name: req.params.text, date: new Date()}
+    },
+  };
+  dynamoDb.put(params, (error) => {
+    if (error){
+    console.log(error);
+  } else {
+    console.log(JSON.stringify(params));
+    res.send(JSON.stringify(params))
+  }
+  })
+
 })
+
 module.exports.handler = serverless(app);
 
