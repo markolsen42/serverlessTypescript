208af00ab0aa05688187a5d6f905cce42a872731
mark olsen <olsen.mark@gmail.com>
2018-02-28 16:58:49 +1300

    add some types and interface to get rid of ts warnings

--- src/Config.ts
+++ src/Config.ts
@@ -1,8 +1,12 @@
 import * as fs from "fs";
 import * as yaml from "js-yaml";
 
+export interface IConfig {
+    postgres: string;
+}
+
 export class Config {
-    public values;
+    public values: IConfig: ;
     public constructor() {
         this.values = yaml.safeLoad(fs.readFileSync("./config.yml", "utf8"));
     }
